b62d7220617b5d24b356ecb379f9155e0eb922f5
kavyaravikanti-kavya <69527626+kavyaravikanti-kavya@users.noreply.github.com>
2020-08-11 23:27:52 +0530

    created program.py

---
+++ program.py
@@ -0,0 +1,18 @@
+list1=[1,"c","c++",9]
+list2=[4,"python",7,8]
+list1.append("java")
+list2.append("python3")
+print(list1,list2)
+
+tupple=("ravi",1,6,"kiran")
+print(tupple[2])
+
+dict={
+"mommy":"mom",
+"daddy":"dad",
+"sister":"sis",
+"brother":"bro"
+}
+del dict["sister"]
+print(dict)
+
